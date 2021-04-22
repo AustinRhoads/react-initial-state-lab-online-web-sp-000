ba5b04a56faef08f860e71133822c5fa66551f74
Austin Rhoads <austinrrhoads@ymail.com>
2021-04-22 11:30:50 -0500

    commit tree 9cd54930ecd0e2a9adc5813ec40e5ec4b596ca23

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,27 @@
 // your Bomb code here!
+import React from 'react'
+
+export default class Bomb extends React.Component{
+
+    constructor(props){
+        super(props)
+        this.state={
+            secondsLeft: props.initialCount
+        }
+
+    }
+
+    div = () => {
+        return this.state.secondsLeft === 0 ? 'Boom!':`${this.state.secondsLeft} seconds left before I go boom!`
+    }
+
+    render(){
+
+        console.log(this.div())
+        return(
+           <div>{this.div()}</div>
+        )
+    }
+
+ 
+}
